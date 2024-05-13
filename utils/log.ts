133c63b860e3dcb079a1e39706697bcaebb767ce
contigen <69436315+contigen@users.noreply.github.com>
2024-05-13 11:17:08 +0100

    Update log helper function

--- utils/log.ts
+++ utils/log.ts
@@ -3,3 +3,7 @@ import { appendFile } from 'fs-extra'
 export async function addErrorToLog(entry: string) {
   await appendFile(`error.txt`, `${entry} at ${new Date()} \n \n`)
 }
+
+export async function addInfoToLog(entry: string) {
+  await appendFile(`info.txt`, `${entry} at ${new Date()} \n \n`)
+}
