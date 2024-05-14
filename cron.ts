1f5b6a1e2a2f6bb4f4394fe5af6935a318ba1c80
contigen <69436315+contigen@users.noreply.github.com>
2024-05-14 19:28:35 +0100

    Remove needless logs

--- cron.ts
+++ cron.ts
@@ -4,14 +4,11 @@ import { addErrorToLog, addInfoToLog } from './utils/log'
 
 cron.schedule(`0 0,6,12,18 * * *`, () => {
   const command = `bun run index.ts`
-  exec(command, (err, stdout, stderr) => {
+  exec(command, (err, stdout) => {
     if (err) {
       addErrorToLog(`Error executing script: ${err}`)
       return
     }
     addInfoToLog(`Script output: ${stdout}`)
-    addErrorToLog(`Script errors: ${stderr}`)
   })
 })
-
-addInfoToLog(`Scheduled task to run four times a day.`)
